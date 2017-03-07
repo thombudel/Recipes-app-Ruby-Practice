cd124aec73c7d2d3097660e1dd53deaf64ee3c53
thom budel <thombudel@gmail.com>
2017-03-07 16:03:50 +0100

    Ask user for ingredients

--- recipe.rb
+++ recipe.rb
@@ -29,3 +29,25 @@ puts"Please go to the Albert Heijn ASAP and get me: "
 ingredients.each do |ingredient|
   puts "#{ingredient[:quantity]} #{ingredient[:name]}"
 end
+
+print_progress_bar
+print_progress_bar
+print_progress_bar
+
+loop do
+  puts "That's quick, do you have all the ingredients? (YES or NO) "
+  answer=gets.chomp.upcase
+
+    if answer=="YES"
+      puts "Great, let's start cooking!"
+      break
+    elsif answer=="NO"
+      puts "OH?! Please go back and get it!"
+      print_progress_bar
+      print_progress_bar
+      print_progress_bar
+    else
+      puts "Please say Yes or No to me!!"
+      print_progress_bar
+  end
+end
